35a3f07b996f61cf68ad76d112b90b2d98d84d8f
Gustavo Barbosa <gusbdev@gmail.com>
2022-07-14 16:55:00 -0300

    feat: select input component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,11 +3,14 @@ import React from "react"
 import ContentHeader from "@/components/ContentHeader"
 
 import { Container } from "./styles"
+import SelectInput from "@/components/SelectInput"
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <ContentHeader />
+      <ContentHeader title="Dashboard" lineColor="#fff">
+        <SelectInput options={[]} />
+      </ContentHeader>
     </Container>
   )
 }
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,17 @@
+import ContentHeader from "@/components/ContentHeader"
+import SelectInput from "@/components/SelectInput"
 import React from "react"
 
+import { Container } from "./styles"
+
 const List: React.FC = () => {
-  return <h1>List</h1>
+  return (
+    <Container>
+      <ContentHeader title="Dashboard" lineColor="#fff">
+        <SelectInput options={[]} />
+      </ContentHeader>
+    </Container>
+  )
 }
 
 export default List
