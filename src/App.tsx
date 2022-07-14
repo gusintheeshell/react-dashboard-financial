f227421aaf812150be4b93f6db7b15ec953e11a3
Gustavo Barbosa <gusbdev@gmail.com>
2022-07-14 11:06:56 -0300

    feat: global theme, light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react"
-import GlobalStyles from "./styles/GlobalStyles"
+import { ThemeProvider } from "styled-components"
 
+import GlobalStyles from "./styles/GlobalStyles"
 import Layout from "./components/Layout"
+
+import light from "./styles/themes/light"
+import dark from "./styles/themes/dark"
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   )
 }
 
