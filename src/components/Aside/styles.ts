6a00b9e51525c696292446347ca4b790af059ada
Gustavo Barbosa <gusbdev@gmail.com>
2022-07-15 12:07:11 -0300

    refactor: remove menu item link

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -5,6 +5,26 @@ export const Container = styled.div`
   background-color: ${(props) => props.theme.colors.secondary};
   padding-left: 20px;
   border-right: 1px solid ${(props) => props.theme.colors.gray};
+
+  a {
+    color: ${(props) => props.theme.colors.info};
+    text-decoration: none;
+
+    margin: 7px 0;
+    display: flex;
+    align-items: center;
+
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    > svg {
+      font-size: 18px;
+      margin-right: 5px;
+    }
+  }
 `
 
 export const Header = styled.header`
@@ -24,23 +44,4 @@ export const MenuContainer = styled.nav`
   display: flex;
   flex-direction: column;
   margin-top: 50px;
-`
-export const MenuItemLink = styled.a`
-  color: ${(props) => props.theme.colors.info};
-  text-decoration: none;
-
-  margin: 7px 0;
-  display: flex;
-  align-items: center;
-
-  transition: opacity 0.3s;
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  > svg {
-    font-size: 18px;
-    margin-right: 5px;
-  }
 `
