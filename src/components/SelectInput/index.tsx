aa1f42254a63ffae79afdae2eef456bcfff5efe1
Gustavo Barbosa <gusbdev@gmail.com>
2022-07-16 15:27:48 -0300

    feat: default value prop

--- src/components/SelectInput/index.tsx
+++ src/components/SelectInput/index.tsx
@@ -8,12 +8,17 @@ interface ISelectInputProps {
     label: string | number
   }[]
   onChange(event: React.ChangeEvent<HTMLSelectElement>): void | undefined
+  defaultValue?: string | number
 }
 
-const SelectInput: React.FC<ISelectInputProps> = ({ options, onChange }) => {
+const SelectInput: React.FC<ISelectInputProps> = ({
+  options,
+  onChange,
+  defaultValue,
+}) => {
   return (
     <Container>
-      <select onChange={onChange}>
+      <select onChange={onChange} defaultValue={defaultValue}>
         {options.map((option) => (
           <option value={option.value}>{option.label}</option>
         ))}
