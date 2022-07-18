34c1f8b3cf7d6c232fab4e02936560cdc5b7fc11
Gustavo Barbosa <gusbdev@gmail.com>
2022-07-18 08:50:48 -0300

    feat: dynamic lost

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react"
 import { useParams } from "react-router-dom"
+import { uuid } from "uuidv4"
 import ContentHeader from "@/components/ContentHeader"
 import HistoryFinanceCard from "@/components/HistoryFinanceCard"
 import SelectInput from "@/components/SelectInput"
@@ -11,6 +12,7 @@ import expenses from "@/repositories/expenses"
 
 import formatCurrency from "@/utils/formatCurrency"
 import formatDate from "@/utils/formatDate"
+import listOfMonths from "@/utils/months"
 
 interface IData {
   id: string
@@ -40,43 +42,57 @@ const List: React.FC = () => {
     return type === "entry-balance" ? "#F7931B" : "#E44C4E"
   }, [type])
 
-  const months = [
-    {
-      value: 7,
-      label: "Julho",
-    },
-    {
-      value: 8,
-      label: "Agosto",
-    },
-    {
-      value: 9,
-      label: "Setembro",
-    },
-  ]
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      }
+    })
+  }, [])
 
-  const years = [
-    { value: 2020, label: 2020 },
-    { value: 2021, label: 2021 },
-    { value: 2022, label: 2022 },
-  ]
+  const years = useMemo(() => {
+    let uniqueYears: number[] = []
+
+    listData.forEach((item) => {
+      const date = new Date(item.date)
+      const year = date.getFullYear()
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year)
+      }
+    })
+
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      }
+    })
+  }, [listData])
 
   useEffect(() => {
-    const response = listData.map(
-      ({ description, frequency, amount, date, type }) => {
-        return {
-          id: String(Math.random() * data.length),
-          description,
-          formattedAmount: formatCurrency(Number(amount)),
-          frequency,
-          formattedDate: formatDate(date),
-          tagColor: frequency === "recorrente" ? "#4E41F0" : "#E66C4E",
-        }
-      }
-    )
+    const filteredData = listData.filter((item) => {
+      const date = new Date(item.date)
+      const month = String(date.getMonth() + 1)
+      const year = String(date.getFullYear())
 
-    setData(response)
-  }, [type])
+      return month === monthSelected && year === yearSelected
+    })
+
+    const formattedDate = filteredData.map((item) => {
+      return {
+        id: uuid(),
+        description: item.description,
+        formattedAmount: formatCurrency(Number(item.amount)),
+        frequency: item.frequency,
+        formattedDate: formatDate(item.date),
+        tagColor: item.frequency === "recorrente" ? "#4E41F0" : "#E66C4E",
+      }
+    })
+
+    setData(formattedDate)
+  }, [listData, monthSelected, yearSelected, data.length])
 
   return (
     <Container>
@@ -84,10 +100,15 @@ const List: React.FC = () => {
         <SelectInput
           options={months}
           onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
         />
         <SelectInput
           options={years}
-          onChange={(e) => setYearSelected(e.target.value)}
+          onChange={(e) => {
+            setYearSelected(e.target.value)
+            console.log(e.target.value)
+          }}
+          defaultValue={yearSelected}
         />
       </ContentHeader>
 
